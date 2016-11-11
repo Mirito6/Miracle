1cf22c7e1a06478d87ef7c1e2d9484a4baef4b0b
MiguelRc <miguel.rc610@gmail.com>
2016-11-11 12:27:32 -0500

    Procesar usuario

---
+++ procesar_usuario.php
@@ -0,0 +1,16 @@
+<?php
+include 'ini.php';
+#Entrada
+
+$info = $_POST['info'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+#Proceso
+#Encriptar el password
+$password_hash = md5($password);
+
+#Guardar el usuario en la base de datos
+#mysql_connect('localhost', 'root', '');
+#mysql_select_db('gamestore');
+$query = "INSERT INTO usuarios VALUES (NULL, '$info', '$email', '$password_hash')";
